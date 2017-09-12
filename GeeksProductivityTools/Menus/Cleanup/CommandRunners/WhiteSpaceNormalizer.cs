7eefb0b7aa76b7a36a429a8d7e71a0dd575595fb
alireza tabatabaee <sa.tabatabaee@live.com>
2017-09-11 23:46:39 -0700

    Final => "If you find a method that only has a single return statement and the body of that method plus the full signature of the method itself together will be less than 100 chars then automatically change it to expression bodied. The same for a read-only property."

--- GeeksProductivityTools/Menus/Cleanup/CommandRunners/WhiteSpaceNormalizer.cs
+++ GeeksProductivityTools/Menus/Cleanup/CommandRunners/WhiteSpaceNormalizer.cs
@@ -14,6 +14,10 @@ namespace Geeks.GeeksProductivityTools.Menus.Cleanup
     public class WhiteSpaceNormalizer : ICodeCleaner
     {
         public void Run(ProjectItem item) => Task.Run(() => NormalizeWhiteSpace(item));
+        //{
+        //    NormalizeWhiteSpace(item);
+        //    //Task.Run(() => NormalizeWhiteSpace(item));
+        //}
 
         static void NormalizeWhiteSpace(ProjectItem item)
         {
@@ -21,7 +25,7 @@ namespace Geeks.GeeksProductivityTools.Menus.Cleanup
 
             initialSource = Formatter.Format(initialSource, GeeksProductivityToolsPackage.Instance.VsWorkspace);
 
-            initialSource = RevomeDuplicaterBlank(initialSource);
+            initialSource = RevomeDuplicaterBlank(initialSource);//.NormalizeWhitespace();
 
             initialSource.WriteSourceTo(item.ToFullPathPropertyValue());
         }
@@ -84,6 +88,11 @@ namespace Geeks.GeeksProductivityTools.Menus.Cleanup
                     _lastMthodDeclarationNode = null;
                 }
 
+                if (node is PropertyDeclarationSyntax)
+                {
+                    node = CheckForReadOnlyProperties(node as PropertyDeclarationSyntax);
+                }
+
                 return base.Visit(node);
             }
 
@@ -130,6 +139,7 @@ namespace Geeks.GeeksProductivityTools.Menus.Cleanup
             }
             SyntaxNode CheckMethodDeclaration(SyntaxNode node)
             {
+                node = CheckMethodForSingleReturnStatement(node as MethodDeclarationSyntax);
                 if (_lastMthodDeclarationNode != null)
                 {
                     var leadingTrivia = CleanUpList(node.GetLeadingTrivia().ToList(), 1);
@@ -140,6 +150,97 @@ namespace Geeks.GeeksProductivityTools.Menus.Cleanup
                 _lastMthodDeclarationNode = node as MethodDeclarationSyntax;
 
                 return node;
+            }
+
+            static SyntaxTrivia[] _spaceTrivia = { SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, " ") };
+
+            MethodDeclarationSyntax CheckMethodForSingleReturnStatement(MethodDeclarationSyntax methodDeclaration)
+            {
+                if ((methodDeclaration.ReturnType is PredefinedTypeSyntax) && (methodDeclaration.ReturnType as PredefinedTypeSyntax).Keyword.IsKind(SyntaxKind.VoidKeyword)) return methodDeclaration;
+                if (methodDeclaration.Body == null) return methodDeclaration;
+                if (methodDeclaration.Body.Statements.Count > 1) return methodDeclaration;
+                var returnStatements = methodDeclaration.Body.Statements.OfType<ReturnStatementSyntax>();
+                if (returnStatements.Count() != 1) return methodDeclaration;
+                var expression = returnStatements.First().Expression.WithoutLeadingTrivia();
+                var length =
+                    expression.WithoutTrivia().Span.Length +
+                    methodDeclaration.Span.Length -
+                    methodDeclaration.Body.FullSpan.Length;
+
+                if (length >= 100) return methodDeclaration;
+
+                var closeParen = methodDeclaration.DescendantTokens().FirstOrDefault(x => x.IsKind(SyntaxKind.CloseParenToken));
+                if (closeParen != null)
+                {
+                    methodDeclaration = methodDeclaration.ReplaceToken(closeParen, closeParen.WithTrailingTrivia(_spaceTrivia));
+                }
+
+                var newMethod =
+                    methodDeclaration
+                        .WithLeadingTrivia(methodDeclaration.GetLeadingTrivia())
+                        .WithExpressionBody(SyntaxFactory.ArrowExpressionClause(expression.WithLeadingTrivia(_spaceTrivia)))
+                        .WithBody(null)
+                        .WithSemicolonToken(GetSemicolon(methodDeclaration.Body))
+                        .WithAdditionalAnnotations(Formatter.Annotation);
+
+
+                return newMethod;
+            }
+            PropertyDeclarationSyntax CheckForReadOnlyProperties(PropertyDeclarationSyntax propertyDeclaration)
+            {
+                if (propertyDeclaration.AccessorList == null) return propertyDeclaration;
+                var getNode = propertyDeclaration.AccessorList.Accessors.FirstOrDefault(x => x.Keyword.IsKind(SyntaxKind.GetKeyword));
+                var setNode = propertyDeclaration.AccessorList.Accessors.FirstOrDefault(x => x.Keyword.IsKind(SyntaxKind.SetKeyword));
+                if (setNode != null || getNode.Body == null) return propertyDeclaration;
+                if (getNode.Body == null) return propertyDeclaration;
+                if (getNode.Body.Statements.Count > 1) return propertyDeclaration;
+
+                var returnStatements = getNode.Body.Statements.OfType<ReturnStatementSyntax>().ToList();
+                if (returnStatements.Count() != 1) return propertyDeclaration;
+                var expression = returnStatements.First().Expression.WithoutTrivia();
+
+                var length =
+                    expression.Span.Length +
+                    propertyDeclaration.Span.Length -
+                    propertyDeclaration.AccessorList.FullSpan.Length;
+
+                if (length >= 100) return propertyDeclaration;
+
+                propertyDeclaration =
+                    propertyDeclaration
+                        .WithIdentifier(propertyDeclaration.Identifier.WithTrailingTrivia(_spaceTrivia))
+                        .WithLeadingTrivia(propertyDeclaration.GetLeadingTrivia())
+                        .WithExpressionBody(SyntaxFactory.ArrowExpressionClause(expression.WithLeadingTrivia(_spaceTrivia)))
+                        .WithAccessorList(null)
+                        .WithSemicolonToken(GetSemicolon(propertyDeclaration.AccessorList))
+                        .WithAdditionalAnnotations(Formatter.Annotation);
+
+                return propertyDeclaration;
+            }
+
+            SyntaxToken GetSemicolon(BlockSyntax block)
+            {
+                var semicolon = ((ReturnStatementSyntax)block.Statements[0]).SemicolonToken;
+
+                var trivia = semicolon.TrailingTrivia.AsEnumerable();
+                trivia = trivia.Where(t => !t.IsKind(SyntaxKind.EndOfLineTrivia));
+
+                var closeBraceTrivia = block.CloseBraceToken.TrailingTrivia.AsEnumerable();
+                trivia = trivia.Concat(closeBraceTrivia);
+
+                return semicolon.WithTrailingTrivia(trivia);
+            }
+            SyntaxToken GetSemicolon(AccessorListSyntax accessorList)
+            {
+                var semicolon = ((ReturnStatementSyntax)accessorList.Accessors[0].Body.Statements[0]).SemicolonToken;
+
+                var trivia = semicolon.TrailingTrivia.AsEnumerable();
+                trivia = trivia.Where(t => !t.IsKind(SyntaxKind.EndOfLineTrivia));
+
+                var closeBraceTrivia = accessorList.CloseBraceToken.TrailingTrivia.AsEnumerable();
+                trivia = trivia.Concat(closeBraceTrivia);
+
+                return semicolon.WithTrailingTrivia(trivia);
             }
             IList<SyntaxTrivia> CleanUpList(IList<SyntaxTrivia> newList)
             {
@@ -174,6 +275,7 @@ namespace Geeks.GeeksProductivityTools.Menus.Cleanup
             {
                 return CleanUpList(syntaxTrivias, 1);
             }
+
             IList<SyntaxTrivia> CleanUpOpenBrace(IList<SyntaxTrivia> syntaxTrivias)
             {
                 return ProcessSpecialTrivias(CleanUpList(syntaxTrivias, 0), itsForCloseBrace: false);
