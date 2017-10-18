400e0bc349fe398bc34ccc7e79f42bb9e09c36dd
alireza tabatabaee <sa.tabatabaee@live.com>
2017-10-18 10:05:22 +0330

    If a file's name ends in .Designer.cs   then all rules should be skipped

--- GeeksProductivityTools/Extensions/FileExtensions.cs
+++ GeeksProductivityTools/Extensions/FileExtensions.cs
@@ -8,5 +8,9 @@ namespace Geeks.GeeksProductivityTools.Extensions
         {
             return projectItem.Kind == Constants.vsProjectItemKindPhysicalFile && projectItem.Name.EndsWith(".cs");
         }
+        public static bool IsCSharpDesignerFile(this ProjectItem projectItem)
+        {
+            return projectItem.Kind == Constants.vsProjectItemKindPhysicalFile && projectItem.Name.EndsWith(".Designer.cs");
+        }        
     }
 }
--- GeeksProductivityTools/Menus/ActionsOnCSharp/ActionCSharpOnProjectItem.cs
+++ GeeksProductivityTools/Menus/ActionsOnCSharp/ActionCSharpOnProjectItem.cs
@@ -8,7 +8,7 @@ namespace Geeks.GeeksProductivityTools.Menus.ActionsOnCSharp
     {
         public static void Action(ProjectItem item, TargetAction targetAction, Definition.CodeCleanerType type)
         {
-            if (item.IsCsharpFile()) targetAction(item, type);
+            if (item.IsCsharpFile() && !item.IsCSharpDesignerFile()) targetAction(item, type);
 
             if (item.ProjectItems == null) return;
 
